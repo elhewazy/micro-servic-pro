1bc280d100e39c2015411a2370d1d9f37e121e28
Obaid Ahmed <138173339+obaidahmed13@users.noreply.github.com>
2024-06-21 18:01:15 -0400

    getBookByCatalog

--- book-service/src/main/java/com/genspark/book_service/repositories/BookRepository.java
+++ book-service/src/main/java/com/genspark/book_service/repositories/BookRepository.java
@@ -2,10 +2,14 @@ package com.genspark.book_service.repositories;
 
 import com.genspark.book_service.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("SELECT book FROM Book book JOIN b.catalogIds c WHERE c = :catalogId")
+    List<Book> findAllByCatalogId(@Param("catalogId") Long catalogId);
 }
