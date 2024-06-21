57b46c39721fe953d2d9c42b7a202fc65e2f4e45
Hy <hyhuynh222@gmail.com>
2024-06-21 13:04:23 -0700

    Added Service and Repository

--- catalog-service/src/main/java/com/genspark/inventory/model/Catalog.java
+++ catalog-service/src/main/java/com/genspark/inventory/model/Catalog.java
@@ -7,19 +7,25 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-
+@ToString
 public class Catalog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String category;
+    private List<Book> books = new ArrayList<>();
 
     public Catalog(String category){
         this.category = category;
     }
+
+
 }
--- catalog-service/src/main/java/com/genspark/inventory/services/CatelogService.java
+++
@@ -1,5 +0,0 @@
-package com.genspark.inventory.services;
-
-public interface CatelogService {
-
-}
